3283e933d9cf97444c8af3b13b0995a5a437a2bc
sromero <sergeromero@users.noreply.github.com>
2017-03-09 16:54:06 -0500

    Added a single unit test from a backend file to make sure both client and server side unit tests are run.

---
+++ src/app/DAL/gameRepository.spec.js
@@ -0,0 +1,9 @@
+const gameRepository = require('./gameRepository');
+
+describe("gemeRepository", () => {
+    it("returns a game", () => {
+        const game = gameRepository.getGame(0);
+
+        return game.then(g => expect(g.title).toBe("The Dark House"));
+    });
+});
