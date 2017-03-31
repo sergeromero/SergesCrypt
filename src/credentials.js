57a37da93d9289fca371002bbe58f6428a8eef65
sromero <sergeromero@users.noreply.github.com>
2017-03-31 15:42:35 -0400

    Story B) - 2. Added the necessary npm packages to handle cookies and sessions.

---
+++ src/credentials.js
@@ -0,0 +1,8 @@
+//Typically this file would not be included in a public source control
+//environment like GitHub.
+
+'use strict';
+
+module.exports = {
+    cookieSecret: 'some cookie secret'
+};
--- src/crypt.js
+++ src/crypt.js
@@ -1,5 +1,10 @@
 var express = require('express');
 
+var app = express();
+
+app.use(express.static(`${__dirname}/resources`));
+app.use(express.static(`${__dirname}/app/Presentation`));
+
 var handlebars = require('express-handlebars').create({
     defaultLayout: 'main',
         helpers: {
@@ -12,17 +17,22 @@ var handlebars = require('express-handlebars').create({
         }
 });
 
-var app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3033);
 
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret
+}));
+
+var credentials = require('./credentials');
 var home = require('./routes/home');
 var adventure = require('./routes/adventure');
 var gameService = require('./routes/gameService');
-
-app.use(express.static(`${__dirname}/resources`));
-app.use(express.static(`${__dirname}/app/Presentation`));
 
 app.use('/adventure', adventure);
 app.use('/game', gameService);
